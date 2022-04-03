b61dde503b875440f4050f5626833da656e28e28
git K3n-sudo <85664243+sudo-keyskip@users.noreply.github.com>
2022-04-03 14:47:48 +0700

    ywer

--- home.py
+++ home.py
@@ -5,20 +5,20 @@ from flask import Flask, render_template
 
 app = Flask (__name__)
 
-@app.route('/welcome')
-def welcome():
+@app.route('/')
+def index():
     return render_template ('index.html')
 
 @app.route('/home')
-def home():
+def index1():
     return render_template ('index1.html')
 
 @app.route('/about')
-def about():
+def index2():
     return render_template ('index2.html')
 
 @app.route('/credits')
-def credits():
+def index3():
     return render_template ('index3.html')
 
 if  __name__ == '__name__' :
